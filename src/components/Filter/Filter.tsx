1752766df6946d7f1f1f4dd4ace88b83f3993373
Iago Guinossi <iagoguinossi@gmail.com>
2022-11-03 18:25:25 -0300

    refactor: add styled-components

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,17 +1,37 @@
 import React from "react";
-import './Filter.css'
+import "./Filter.css";
+import styled from "styled-components";
 
-export function Filter(){
-function handleSubmit(e: any){
-    e.preventDefault()
-}
+const ContainerForm = styled.form`
+  background-color: white;
+  display: flex;
+`;
 
-    return (
-        <div className="">
-        <form onSubmit={handleSubmit} className='search-box'>
-            <input className="input-search" placeholder='Search your Pokemon'/>
-            <button className="button-search"/>
-        </form>
-        </div>
-    )
+const TextInput = styled.input`
+  background-color: transparent;
+  outline: none;
+  border: none;
+
+  &::placeholder {
+    color: gray;
+  }
+`;
+
+const SubmitButton = styled.button`
+  border-radius: 100%;
+  height: 25px;
+  width: 25px;
+`;
+
+export function Filter() {
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+  }
+
+  return (
+    <ContainerForm onSubmit={(e) => {}}>
+      <TextInput placeholder="Search your Pokemon" />
+      <SubmitButton />
+    </ContainerForm>
+  );
 }
