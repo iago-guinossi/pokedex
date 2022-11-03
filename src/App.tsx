ffc5efe2c58c61f0b50ab97dbb7f1411c0493cc7
Iago Guinossi <iagoguinossi@gmail.com>
2022-11-03 18:50:53 -0300

    refactor and add themeprovider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import './App.css'
-import { Filter } from './components/Filter'
+import "./App.css";
+import { Filter } from "./components/Filter";
+import { ThemeProvider } from "styled-components";
+import { THEME_RAIOROS } from "./styles";
+
+
 
 function App() {
   return (
-    <Filter/>
-  )
+    <ThemeProvider theme={THEME_RAIOROS}>
+      <Filter />
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
