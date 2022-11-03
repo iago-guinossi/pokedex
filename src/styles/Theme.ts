760df8c66fba2159b8dd283ee8cab7419fc8c5a9
Iago Guinossi <iagoguinossi@gmail.com>
2022-11-03 18:51:14 -0300

    add Theme

---
+++ src/styles/Theme.ts
@@ -0,0 +1,16 @@
+export const THEME_RAIOROS = <const>{
+  raioros: true,
+  color: {
+    background: "#fff",
+  },
+  font: {
+    size: {
+      sm: 8,
+      md: 12,
+      lg: 122,
+    },
+    family: 'asd, asd, "asd asd", asd',
+  },
+};
+
+export type ThemeRaioros = typeof THEME_RAIOROS;
