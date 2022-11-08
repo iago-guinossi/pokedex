9a4f8afeaa9c3bb689ce81ac0e2a5f0e009ca8ca
Iago Guinossi <iagoguinossi@gmail.com>
2022-11-08 11:37:12 -0300

    Create cardInfo

---
+++ src/components/CardInfo/CardInfo.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const CardInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  box-sizing: border-box;
+  padding-bottom: 30px;
+  width: 100%;
+  height: 210px;
+  border-radius: 25px;
+  background-color: white;
+`;
