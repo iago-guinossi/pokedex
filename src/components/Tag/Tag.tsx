cc334f18d9ab62073e80f82ff7fef35935fcad6b
Iago Guinossi <iagoguinossi@gmail.com>
2022-11-08 11:37:46 -0300

    Create tag

---
+++ src/components/Tag/Tag.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+type TagProps = {
+  color: string;
+};
+
+export const Tag = styled.span<TagProps>`
+  text-transform: uppercase;
+  font-size: 12px;
+  background-color: ${({color}) => color};
+  padding: 8px;
+  color: rgba(0,0,0,0.5);
+  font-weight: bold;
+  font-family:'arial black';
+  border: none;
+  border-radius: 8px;
+  margin: 0 5px;
+`;
