2f8b3c1e20ebcd8d7d2ca43429f583620813b988
Iago Guinossi <iagoguinossi@gmail.com>
2022-11-08 11:38:33 -0300

    Create Name Pokemon

---
+++ src/components/NamePokemon/NamePokemon.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const NamePokemon = styled.span`
+    margin-bottom: 15px;
+    font-size: 18px;
+    font-weight: bold;
+    font-family: 'arial black';
+`
