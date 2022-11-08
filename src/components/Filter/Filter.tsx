a19f2392b5a2fafba09a5562bed0e862a2024b91
Iago Guinossi <iagoguinossi@gmail.com>
2022-11-08 11:38:51 -0300

    Create Filter

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,19 +1,21 @@
-import React, { AnchorHTMLAttributes } from "react";
+import React from "react";
 import styled from "styled-components";
+import { PokemonTypeEnum } from "../../domain/types";
+import { PokemonType } from "../PokemonType";
 
 const Container = styled.form`
-${({ theme }) => `
+  ${({ theme }) => `
     background-color: ${theme.color.background};
     `}
   display: flex;
-  align-items:center;
+  align-items: center;
   justify-content: space-between;
   height: 90px;
   width: 950px;
   padding: 0 25px;
   box-sizing: border-box;
   border-radius: 15px;
-  box-shadow: 10px 10px 15px rgba(255,255,255,0.4);
+  box-shadow: 10px 10px 15px rgba(255, 255, 255, 0.4);
 `;
 
 const Text = styled.input`
@@ -39,11 +41,7 @@ const Submit = styled.button`
   box-shadow: 15px 15px 20px rgba(255, 83, 80, 0.6);
 `;
 
-const Container2 = styled.div``
-
-const ContainerFilters = styled.div``
-
-const SelectFilter = styled.select``
+const Container2 = styled.div``;
 
 export function Filter() {
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
@@ -52,32 +50,28 @@ export function Filter() {
 
   return (
     <Container2>
-    <Container onSubmit={handleSubmit}>
-      <Text placeholder="Search your Pokemon!" />
-      <Submit />
-    </Container>
-    <ContainerFilters>
-      <SelectFilter>
-        <option>Normal</option>
-        <option>Fire</option>
-        <option>Water</option>
-        <option>Grass</option>
-        <option>Electric</option>
-        <option>Ice</option>
-        <option>Fighting</option>
-        <option>Poison</option>
-        <option>Ground</option>
-        <option>Flying</option>
-        <option>Psychic</option>
-        <option>Bug</option>
-        <option>Rock</option>
-        <option>Ghost</option>
-        <option>Dark</option>
-        <option>Dragon</option>
-        <option>Steel</option>
-        <option>Fairy</option>
-      </SelectFilter>
-    </ContainerFilters>
+      <Container onSubmit={handleSubmit}>
+        <Text placeholder="Search your Pokemon!" />
+        <Submit />
+      </Container>
+      <PokemonType type={PokemonTypeEnum.Normal} />
+      <PokemonType type={PokemonTypeEnum.Bug} />
+      <PokemonType type={PokemonTypeEnum.Water} />
+      <PokemonType type={PokemonTypeEnum.Grass} />
+      <PokemonType type={PokemonTypeEnum.Electric} />
+      <PokemonType type={PokemonTypeEnum.Ice} />
+      <PokemonType type={PokemonTypeEnum.Fighting} />
+      <PokemonType type={PokemonTypeEnum.Poison} />
+      <PokemonType type={PokemonTypeEnum.Ground} />
+      <PokemonType type={PokemonTypeEnum.Flying} />
+      <PokemonType type={PokemonTypeEnum.Psychic} />
+      <PokemonType type={PokemonTypeEnum.Fire} />
+      <PokemonType type={PokemonTypeEnum.Rock} />
+      <PokemonType type={PokemonTypeEnum.Ghost} />
+      <PokemonType type={PokemonTypeEnum.Dark} />
+      <PokemonType type={PokemonTypeEnum.Dragon} />
+      <PokemonType type={PokemonTypeEnum.Steel} />
+      <PokemonType type={PokemonTypeEnum.Fairy} />
     </Container2>
   );
 }
