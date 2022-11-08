a57d92cc8f71be63c30fc0ad0d55d7c7ec66385f
Iago Guinossi <iagoguinossi@gmail.com>
2022-11-08 11:39:15 -0300

    Add Card and Pokemon info on app

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@
 import { Filter } from "./components/Filter";
 import { ThemeProvider } from "styled-components";
 import { THEME_RAIOROS } from "./styles";
-
-
+import { Card } from "./components/Card";
+import { PokeInfo} from "./components/PokeInfo"
 
 function App() {
   return (
     <ThemeProvider theme={THEME_RAIOROS}>
       <Filter />
+      <Card />
+      <PokeInfo/>
     </ThemeProvider>
   );
 }
