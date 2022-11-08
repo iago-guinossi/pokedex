7fa657106b407d8cc9de922fedbd131a00556f7f
Iago Guinossi <iagoguinossi@gmail.com>
2022-11-08 17:54:56 -0300

    Refactor

--- src/components/PokeInfo/PokeInfo.tsx
+++ src/components/PokeInfo/PokeInfo.tsx
@@ -7,6 +7,14 @@ import { NumberPokemon } from "../NumberPokemon";
 import { PokemonStats } from "../PokemonStats";
 import { PokemonType } from "../PokemonType";
 import { Tag } from "../uiComponents/Tag";
+import { PokemonTypeRounded } from "../PokemonTypeRounded";
+import { TagRounded } from "../uiComponents/TagRounded";
+import { Weaknesses } from "../infoComponents/Weaknesses";
+import { PokeAbility } from "../infoComponents/PokeAbility";
+import { PokemonEntry } from "../infoComponents/PokemonEntry";
+import { MiscInfo } from "../infoComponents/MiscInfo";
+import { PokemonEvo } from "../infoComponents/PokemonEvo";
+import { ButtonNextPrev } from "../infoComponents/ButtonNextPrev";
 
 const Container = styled.div`
   background-color: black;
@@ -39,40 +47,6 @@ const PokeTypeContainer = styled.div`
   flex-direction: row;
 `;
 
-const PokeEntry = styled.span``;
-
-const PokeAbility = styled(Tag)`
-  border-radius: 30px;
-`;
-
-const PokeAbilityContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
-
-const SizeContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
-
-const HeightContainer = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-`
-const Height = styled(Tag)`
-border-radius: 30px;
-`
-
-const WeightContainer = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-`
-const Weight = styled(Tag)`
-border-radius: 30px;
-`
-
 export function PokeInfo() {
   return (
     <Container>
@@ -84,31 +58,12 @@ export function PokeInfo() {
           <PokemonType type={PokemonTypeEnum.Grass} />
           <PokemonType type={PokemonTypeEnum.Grass} />
         </PokeTypeContainer>
-        <NamePokemon>Pokedex Entry</NamePokemon>
-        <PokeEntry>
-          When the bulb on\nits back grows\nlarge, it appears\fto lose
-          the\nability to stand\non its hind legs.
-        </PokeEntry>{" "}
-        {/** https://pokeapi.co/api/v2/pokemon-species/{id or name}/ */}
-        <PokeAbilityContainer>
-          <PokeAbility color="lightgray">Overgrow</PokeAbility>{" "}
-          {/** https://pokeapi.co/api/v2/pokemon/2 */}
-          <PokeAbility color="lightgray">chlorophyll</PokeAbility>{" "}
-          {/** https://pokeapi.co/api/v2/pokemon/2 */}
-        </PokeAbilityContainer>
-        <SizeContainer>
-          <HeightContainer>
-            <NamePokemon>Height</NamePokemon>
-            <Height color='lightgray'>10 Decimetros</Height>
-             {/** https://pokeapi.co/api/v2/pokemon/2 */}
-          </HeightContainer>
-          <WeightContainer>
-            <NamePokemon>Width</NamePokemon>
-            <Weight color='lightgray'>130 hectogramas</Weight>
-             {/** https://pokeapi.co/api/v2/pokemon/2 */}
-          </WeightContainer>
-        </SizeContainer>
+        <PokemonEntry/>
+        <PokeAbility/>
+        <MiscInfo/>
         <PokemonStats />
+        <PokemonEvo/>
+        <ButtonNextPrev/>
       </Card>
     </Container>
   );
