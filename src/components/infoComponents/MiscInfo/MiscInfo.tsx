7784d9cd8e8122bbb57e104b842935824717cb88
Iago Guinossi <iagoguinossi@gmail.com>
2022-11-09 14:02:30 -0300

    refactor

--- src/components/infoComponents/MiscInfo/MiscInfo.tsx
+++ src/components/infoComponents/MiscInfo/MiscInfo.tsx
@@ -7,6 +7,8 @@ import { Weaknesses } from "../Weaknesses"
 const MiscGrid = styled.div`
 display:grid;
 grid-template-columns: repeat(2, 1fr);
+row-gap: 10px;
+column-gap: 10px;
 `
 
 export function MiscInfo(){
