43e41a6e245c0fa4fff3e4491697057654833e50
Iago Guinossi <iagoguinossi@gmail.com>
2022-11-09 14:23:57 -0300

    Refactor size

--- src/components/uiComponents/NamePokemon/NamePokemon.tsx
+++ src/components/uiComponents/NamePokemon/NamePokemon.tsx
@@ -1,8 +1,12 @@
 import styled from "styled-components";
 
-export const NamePokemon = styled.span`
+type NamePokemonProps = {
+    size: string;
+  };
+
+export const NamePokemon = styled.span<NamePokemonProps>`
     margin-bottom: 15px;
-    font-size: 18px;
+    font-size: ${({size}) => size};
     font-weight: bold;
     font-family: 'arial black';
 `
