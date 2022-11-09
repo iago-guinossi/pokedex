c4229e2519f6db9e585537733140f578b6ce8473
Iago Guinossi <iagoguinossi@gmail.com>
2022-11-09 13:59:42 -0300

    create title

---
+++ src/components/uiComponents/Title/Title.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Title = styled.span`
+    margin-bottom: 10px;
+    margin-top: 15px;
+    text-transform: uppercase;
+    font-size: 16px;
+    font-weight: bold;
+    font-family: 'arial black';
+`
