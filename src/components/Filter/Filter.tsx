49cafc8ebe148f3d3583a856485f733d4f708435
Iago Guinossi <iagoguinossi@gmail.com>
2022-11-11 16:00:57 -0300

    refactor

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 import { PokemonTypeEnum } from "../../domain/types";
+import { useChosePokemon } from "../Provider/ChosePokemonProvider";
 import { PokemonType } from "../uiComponents/PokemonType";
+import { useId } from "../Provider/ChosePokemonProvider";
 
 const Container = styled.form`
   ${({ theme }) => `
@@ -11,11 +13,12 @@ const Container = styled.form`
   align-items: center;
   justify-content: space-between;
   height: 90px;
-  width: 950px;
+  max-width: 1660px ;
   padding: 0 25px;
   box-sizing: border-box;
   border-radius: 15px;
   box-shadow: 10px 10px 15px rgba(255, 255, 255, 0.4);
+  margin: 20px;
 `;
 
 const Text = styled.input`
@@ -41,37 +44,16 @@ const Submit = styled.button`
   box-shadow: 15px 15px 20px rgba(255, 83, 80, 0.6);
 `;
 
-const Container2 = styled.div``;
-
 export function Filter() {
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
   }
 
   return (
-    <Container2>
+  
       <Container onSubmit={handleSubmit}>
         <Text placeholder="Search your Pokemon!" />
         <Submit />
       </Container>
-      <PokemonType type={PokemonTypeEnum.Normal} />
-      <PokemonType type={PokemonTypeEnum.Bug} />
-      <PokemonType type={PokemonTypeEnum.Water} />
-      <PokemonType type={PokemonTypeEnum.Grass} />
-      <PokemonType type={PokemonTypeEnum.Electric} />
-      <PokemonType type={PokemonTypeEnum.Ice} />
-      <PokemonType type={PokemonTypeEnum.Fighting} />
-      <PokemonType type={PokemonTypeEnum.Poison} />
-      <PokemonType type={PokemonTypeEnum.Ground} />
-      <PokemonType type={PokemonTypeEnum.Flying} />
-      <PokemonType type={PokemonTypeEnum.Psychic} />
-      <PokemonType type={PokemonTypeEnum.Fire} />
-      <PokemonType type={PokemonTypeEnum.Rock} />
-      <PokemonType type={PokemonTypeEnum.Ghost} />
-      <PokemonType type={PokemonTypeEnum.Dark} />
-      <PokemonType type={PokemonTypeEnum.Dragon} />
-      <PokemonType type={PokemonTypeEnum.Steel} />
-      <PokemonType type={PokemonTypeEnum.Fairy} />
-    </Container2>
   );
 }
