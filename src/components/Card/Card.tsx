37dece6b826ddca65feb438b8be4ababde1517d5
Iago Guinossi <iagoguinossi@gmail.com>
2022-11-14 20:52:17 -0300

    Add List

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,15 +1,14 @@
-import React from "react";
+import { useEffect } from "react";
 import styled from "styled-components";
-import { PokemonType } from "../uiComponents/PokemonType";
-import { NumberPokemon } from "../uiComponents/NumberPokemon";
-import { NamePokemon } from "../uiComponents/NamePokemon";
+import { GetPokemonResponse } from "../../domain/pokemon";
+import { usePokemonList } from "../Provider/ListPokemonProvider";
+import { usePokemonDetails } from "../Provider/PokemonDetails/PokemonDetails";
 import { CardInfo } from "../uiComponents/CardInfo";
-import { useQuery } from "react-query";
-import axios from "axios";
-import { usePokemonDetails } from "../Provider/ChosePokemonProvider/PokemonDetails";
+import { NamePokemon } from "../uiComponents/NamePokemon";
+import { NumberPokemon } from "../uiComponents/NumberPokemon";
+import { PokemonType } from "../uiComponents/PokemonType";
 
 const CardExt = styled.div`
-  background-color: black;
   height: 260px;
   width: 300px;
   margin: 20px;
@@ -41,55 +40,14 @@ const Container = styled.div`
 
 const CardInt = styled(CardInfo)`
   padding: 70px 0 30px 0;
-`
+`;
 
-const getPokemons = async (key, limit) => {
-  const {data} = await axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=${limit}`)
-  return data
-}
+type PokemonCardProps = {
+  pokemon: GetPokemonResponse;
+  id: number;
+};
 
-const getPokemon = async (id) => {
-  const {data} = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-  return data
-}
-const id = 2
-const pokemon = await getPokemon(id);
-
-// export function Card() {
-//   const { data, status } = useQuery(["pokemons", 22], getPokemons);
-//   const PokemonCard = (pokemons) => {
-//     console.log(pokemons)
-//     return pokemons.results.map((pokemon)=> {
-//       const poke = getPokemon
-//       return(
-//       <CardExt>
-//         <PokeImg
-//           src={poke.sprites.other.dream_world.front_default}
-//           alt={poke.species.name}
-//         />
-//         <CardInfo>
-//           <NumberPokemon>N°</NumberPokemon>
-//           <NamePokemon size="medium">{poke.species.name}</NamePokemon>
-//           <PokeTypeContainer>
-//             {poke.types.map((type: any, i: number) => {
-//               return <PokemonType key={i} type={type.type.name} />;
-//             })}
-//           </PokeTypeContainer>
-//         </CardInfo>
-//       </CardExt>
-//       )
-//     })
-//   };
-//   return (
-//     <Container>
-//       {status === "loading" && <div>Loading...</div>}
-//       {status === "error" && <div>Error fetching pokemons</div>}
-//       {status === "success" && <div>{PokemonCard(data)}</div>}
-//     </Container>
-//   );
-// }
-
-function List() {
+function PokemonCard({ pokemon, id }: PokemonCardProps) {
   const pokeDetails = usePokemonDetails();
 
   const handleClick = (id: string | number) => {
@@ -97,42 +55,42 @@ function List() {
   };
 
   return (
-    <>
-      {[1, 2, 3, 4].map((i) => (
-        <div onClick={() => handleClick(i)}>poke{i}</div>
-      ))}
-    </>
+    <CardExt key={id} onClick={() => handleClick(id + 1)}>
+      <PokeImg
+        src={pokemon.sprites.other.dream_world.front_default}
+        alt={pokemon.species.name}
+      />
+      <CardInt>
+        <NumberPokemon>N°{id}</NumberPokemon>
+        <NamePokemon size="medium">{pokemon.species.name}</NamePokemon>
+        <PokeTypeContainer>
+          {pokemon.types.map((type: any, i: number) => {
+            return <PokemonType key={i} type={type.type.name} />;
+          })}
+        </PokeTypeContainer>
+      </CardInt>
+    </CardExt>
   );
 }
 
+// TODO: Refactor to List()
+export function List() {
+  const { nextPage, isLoading, pokeList } = usePokemonList();
 
-export function Card() {
-  const pokeDetails = usePokemonDetails();
+  if (isLoading || (pokeList || []).length < 1)
+    return (
+      <>
+        <Container>carrregfamdop</Container>
+        <button onClick={nextPage}>proxima pagina</button>
+      </>
+    );
 
-  const handleClick = (id: string | number) => {
-    pokeDetails?.setPokemon(id);
-  };
   return (
     <Container>
-      <CardExt>
-        <PokeImg
-          src={pokemon.sprites.other.dream_world.front_default}
-          alt={pokemon.species.name}
-        />
-        <CardInt>
-          <NumberPokemon>N°{id}</NumberPokemon>
-          <NamePokemon size="medium">{pokemon.species.name}</NamePokemon>
-          <PokeTypeContainer>
-            {pokemon.types.map((type: any, i: number) => {
-              return <PokemonType key={i} type={type.type.name} />;
-            })}
-          </PokeTypeContainer>
-        </CardInt>
-        <button onClick={() => handleClick(1)}>001</button>
-        <button onClick={() => handleClick(2)}>002</button>
-        <button onClick={() => handleClick(3)}>003</button>
-        <button onClick={() => handleClick(4)}>004</button>
-      </CardExt>
+      {pokeList?.map((poke) => (
+        <PokemonCard pokemon={poke} key={poke.name} id={poke.id} />
+      ))}
+      <button onClick={nextPage}>proxima pagina</button>
     </Container>
   );
 }
