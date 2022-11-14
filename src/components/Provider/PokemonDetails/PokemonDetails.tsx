d8d9b026a19bf03bddc9fe49d91f97a9f1a017a0
Iago Guinossi <iagoguinossi@gmail.com>
2022-11-14 20:54:20 -0300

    Move pokemon details

---
+++ src/components/Provider/PokemonDetails/PokemonDetails.tsx
@@ -0,0 +1,52 @@
+import React, { createContext, useContext, useState } from "react";
+import { pokemonApi } from "../../../Api";
+
+type PokemonDetail = {
+  pokemonInfo: any | null;
+  setPokemon: (pokemon: number | string | null) => Promise<void>;
+  isLoading: boolean;
+};
+
+const PokemonDetailContext = createContext<PokemonDetail | null>(null);
+
+export function PokemonDetailsProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [pokemonInfo, setPokemonInfo] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const setPokemon: PokemonDetail["setPokemon"] = async (identifier) => {
+    if (identifier === null) return setPokemonInfo(null);
+    setIsLoading(true);
+    let pokemon; //fix any, map pokemon api types
+    let pokemonSpecies;
+    let evolutionChain;
+    let pokedex;
+    if (typeof identifier === "number")
+      pokemon = await pokemonApi.fetchPokemonByNumber(identifier);
+    else pokemon = await pokemonApi.fetchPokemonByName(identifier);
+
+    pokemonSpecies = await pokemonApi.fetchPokemonSpecies(pokemon);
+    evolutionChain = await pokemonApi.fetchEvolutionChain(pokemonSpecies);
+    pokedex = await pokemonApi.fetchPokedex();
+
+    setPokemonInfo({
+      pokemon,
+      pokemonSpecies: pokemonSpecies,
+      evolutionChain: evolutionChain,
+      pokedex: pokedex,
+    });
+
+    setIsLoading(false);
+  };
+
+  return (
+    <PokemonDetailContext.Provider value={{ setPokemon, pokemonInfo, isLoading }}>
+      {children}
+    </PokemonDetailContext.Provider>
+  );
+}
+
+export const usePokemonDetails = () => useContext(PokemonDetailContext);
