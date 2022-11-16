139d182015d2e4311afc4ffeaf54034336b54593
Iago Guinossi <iagoguinossi@gmail.com>
2022-11-16 14:43:08 -0300

    add onclick on evolution

--- src/components/infoComponents/PokemonEvo/PokemonEvo.tsx
+++ src/components/infoComponents/PokemonEvo/PokemonEvo.tsx
@@ -8,69 +8,49 @@ const Img = styled.img`
   height: 70px;
   width: 70px;
   margin: 10px;
+  cursor: pointer;
 `;
 const EvoContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  flex-wrap: wrap;
+  width: 100%;
 `;
 
-// const id = 133
-// const id = 2;
+const Evolution = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
 
-// const pokemon = await getPokemon(id);
-
-// const evolution = await getEvolutionChain(pokemon);
-
-// async function getPokemon(pokeNumber: any) {
-//     const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokeNumber}`)
-//     const data = await response.json()
-
-//     return data
-// }
-
-// async function getPokedex() {
-//     const response = await fetch('https://pokeapi.co/api/v2/pokedex/1/')
-//     const data = await response.json()
-
-//     return data
-//   }
-
-// async function getEvolutionChain(pokemon) {
-//     const response = await fetch(pokemon.evolution_chain.url)
-//     const data = await response.json()
-//     return data
-// }
-
-// const pokedex = await getPokedex();
-
-
-// console.log(pokemon)
-// console.log(evolution)
 function renderEvo(evoList) {
-  const pokemonDetails = usePokemonDetails()
-    const pokedex = pokemonDetails?.pokemonInfo.pokedex
-    const sprite = pokedex.pokemon_entries.find(
-        (poke) => poke.pokemon_species.name === evoList.species.name
-        );
-        return (
-            <span key={sprite.entry_number}>
-      <Img
-        src={sprite.entry_number > 649 ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${sprite.entry_number}.png` : `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${sprite.entry_number}.svg`}
+  const pokemonDetails = usePokemonDetails();
+  const pokedex = pokemonDetails?.pokemonInfo.pokedex;
+  const sprite = pokedex.pokemon_entries.find(
+    (poke) => poke.pokemon_species.name === evoList.species.name
+  );
+
+  const handleClick = (id: string | number) => {
+    pokemonDetails?.setPokemon(id);
+  };
+
+  return (
+    <Evolution key={sprite.entry_number}>
+      <Img onClick={() => handleClick(sprite.entry_number)}
+        src={
+          sprite.entry_number > 649
+            ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${sprite.entry_number}.png`
+            : `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${sprite.entry_number}.svg`
+        }
         alt={sprite.pokemon_species.name}
-        />
+      />
       {evoList.evolves_to.length > 0
         ? evoList.evolves_to.map((evo) => renderEvo(evo))
         : null}
-    </span>
+    </Evolution>
   );
 }
 
 export function PokemonEvo() {
-  const pokemonDetails = usePokemonDetails()
-    const evolutionChain = pokemonDetails?.pokemonInfo.evolutionChain
+  const pokemonDetails = usePokemonDetails();
+  const evolutionChain = pokemonDetails?.pokemonInfo.evolutionChain;
   return evolutionChain.chain.evolves_to.length > 0 ? (
     <>
       <Title>Evolution</Title>
