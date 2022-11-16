f909232393d7a492a459a674eac629ce75fcf3a0
Iago Guinossi <iagoguinossi@gmail.com>
2022-11-16 16:38:03 -0300

    Remove StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,5 @@ import './styles/normalize.css'
 import './styles/globals.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 )
